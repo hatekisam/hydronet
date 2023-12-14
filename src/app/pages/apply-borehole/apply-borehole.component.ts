f924b7480b71e63568a1c518490fa776aa212a45
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:16:39 +0200

    Added the apply bolehole page

---
+++ src/app/pages/apply-borehole/apply-borehole.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-borehole',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      apply-borehole works!
+    </p>
+  `,
+  styleUrl: './apply-borehole.component.css'
+})
+export class ApplyBoreholeComponent {
+
+}
