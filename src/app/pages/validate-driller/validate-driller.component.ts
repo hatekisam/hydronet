df2e798bd07539da8110d12fa85b13914a2060d5
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:17:39 +0200

    Added the validate driller

---
+++ src/app/pages/validate-driller/validate-driller.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-driller',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      validate-driller works!
+    </p>
+  `,
+  styleUrl: './validate-driller.component.css'
+})
+export class ValidateDrillerComponent {
+
+}
