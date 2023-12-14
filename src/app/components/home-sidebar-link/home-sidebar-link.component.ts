3c6f8d681f570e732a225e0edd42298919b43bd8
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:31:13 +0200

    Added the home sidebar link icon

---
+++ src/app/components/home-sidebar-link/home-sidebar-link.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-sidebar-link',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      home-sidebar-link works!
+    </p>
+  `,
+  styleUrl: './home-sidebar-link.component.css'
+})
+export class HomeSidebarLinkComponent {
+
+}
