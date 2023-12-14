39d638c77702841525b0e710d46419c1a1b97d55
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:34:00 +0200

    added the home link component

--- src/app/components/home-sidebar-link/home-sidebar-link.component.ts
+++ src/app/components/home-sidebar-link/home-sidebar-link.component.ts
@@ -1,16 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-sidebar-link',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      home-sidebar-link works!
-    </p>
-  `,
-  styleUrl: './home-sidebar-link.component.css'
+  template: ` <p>home-sidebar-link works!</p> `,
 })
 export class HomeSidebarLinkComponent {
-
+  @Input() link;
 }
