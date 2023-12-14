579bd841f7e2192e3fc7e9b2e8895819017f872c
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:16:59 +0200

    Added the request water page

---
+++ src/app/pages/request-water/request-water.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-water',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      request-water works!
+    </p>
+  `,
+  styleUrl: './request-water.component.css'
+})
+export class RequestWaterComponent {
+
+}
