2272f275dd8bc82652876ee4d662cf328482b3fe
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 14:00:49 +0200

    finished the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,12 +10,36 @@ import { Component } from '@angular/core';
       <div class="px-20 py-2">
         <div class="flex justify-between">
           <div class="flex items-center gap-4">
-            <a href="">Home</a>
-            <a href="">About Us</a>
-            <a href="">Mission</a>
-            <a href="">Staffs</a>
-            <a href="">Report</a>
-            <a href="">Contact US</a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/downArrow.svg" alt="" />
+              <p>Home</p>
+            </a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/aboutUs.svg" alt="" />
+              <p>About Us</p>
+            </a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/mission.svg" alt="" />
+              <p>Mission</p>
+            </a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/staff.svg" alt="" />
+              <p>Staffs</p>
+            </a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/report.svg" alt="" />
+              <p>Report</p>
+            </a>
+            <a href="" class="flex gap-2 items-center">
+              <img src="/assets/contact.svg" alt="" />
+              <p>Contact Us</p>
+            </a>
+            <button
+              class="px-4 py-1 bg-[#63CCD3] text-white font-semibold rounded-md flex items-center gap-2"
+            >
+              <img src="/assets/pcPlatform.svg" alt="" />
+              <p>Pc Platfrom</p>
+            </button>
           </div>
           <button
             class="px-4 py-1 bg-[#158BA0] text-white font-semibold rounded-full"
