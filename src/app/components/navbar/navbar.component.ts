7e033c0fa90d0974c2ae7674fe4dbeabe9418a8b
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 12:12:58 +0200

    Did the navbar page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,12 +5,27 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      navbar works!
-    </p>
+    <div class="w-screen">
+      <div class="h-4 w-full bg-[#63CCD3]"></div>
+      <div class="px-20 py-2">
+        <div class="flex justify-between">
+          <div class="flex items-center gap-4">
+            <a href="">Home</a>
+            <a href="">About Us</a>
+            <a href="">Mission</a>
+            <a href="">Staffs</a>
+            <a href="">Report</a>
+            <a href="">Contact US</a>
+          </div>
+          <button
+            class="px-4 py-1 bg-[#158BA0] text-white font-semibold rounded-full"
+          >
+            Fri fragt på alle order over 1000kr
+          </button>
+        </div>
+      </div>
+      <div class="h-3 w-full bg-[#D1F4F7]"></div>
+    </div>
   `,
-  styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
