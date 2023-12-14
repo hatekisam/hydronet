d2d17ed459c9496341b190365a667a9317b5f651
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 12:02:21 +0200

    Added the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  template: `<main>
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  </main>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'hydronet';
