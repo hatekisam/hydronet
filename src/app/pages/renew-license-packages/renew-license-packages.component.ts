1ed7ff4fbc4e7097c20370a09a7aef5668614050
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-15 21:25:24 +0200

    created the select packages

---
+++ src/app/pages/renew-license-packages/renew-license-packages.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renew-license-packages',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      renew-license-packages works!
+    </p>
+  `,
+  styleUrl: './renew-license-packages.component.css'
+})
+export class RenewLicensePackagesComponent {
+
+}
