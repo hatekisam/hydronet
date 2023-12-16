dbd85899c988e162753c33f1c7642edce24dbd79
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-16 17:09:16 +0200

    started the one driller page

---
+++ src/app/pages/one-driller/one-driller.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-driller',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      one-driller works!
+    </p>
+  `,
+  styleUrl: './one-driller.component.css'
+})
+export class OneDrillerComponent {
+
+}
