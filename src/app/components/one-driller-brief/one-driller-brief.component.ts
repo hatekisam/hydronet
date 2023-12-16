d5e45b06a37c979caf621624e99a4a7834b65c26
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-16 18:06:10 +0200

    Added one driller brief

---
+++ src/app/components/one-driller-brief/one-driller-brief.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-driller-brief',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      one-driller-brief works!
+    </p>
+  `,
+  styleUrl: './one-driller-brief.component.css'
+})
+export class OneDrillerBriefComponent {
+
+}
