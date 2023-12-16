22de0c2b31fb5b103349b14f0b0563bd14e22120
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-16 18:20:44 +0200

    added one driller about component

---
+++ src/app/components/one-driller-about/one-driller-about.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-driller-about',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      one-driller-about works!
+    </p>
+  `,
+  styleUrl: './one-driller-about.component.css'
+})
+export class OneDrillerAboutComponent {
+
+}
