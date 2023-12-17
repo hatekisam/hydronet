c16d6d49814f4cac693006fea217b742a4114f46
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-17 15:02:18 +0200

    started doing the circluar progress

---
+++ src/app/components/circlura-progress/circlura-progress.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circlura-progress',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      circlura-progress works!
+    </p>
+  `,
+  styleUrl: './circlura-progress.component.css'
+})
+export class CircluraProgressComponent {
+
+}
