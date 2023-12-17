b54ae456e5c9b413fb0ed9d958b7ee05d7974d19
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-17 12:01:10 +0200

    added the upload file 1

---
+++ src/app/components/upload-file/upload-file.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      upload-file works!
+    </p>
+  `,
+  styleUrl: './upload-file.component.css'
+})
+export class UploadFileComponent {
+
+}
