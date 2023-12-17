82dc24a94d79ac3deddcc6ed695c1570a6482535
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-17 09:51:00 +0200

    added main register company

---
+++ src/app/pages/main-register-company/main-register-company.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-register-company',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      main-register-company works!
+    </p>
+  `,
+  styleUrl: './main-register-company.component.css'
+})
+export class MainRegisterCompanyComponent {
+
+}
