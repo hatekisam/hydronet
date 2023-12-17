dd9386e696c86ada3f3faa2b9e5024626986e1db
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-17 10:01:45 +0200

    added the main register user accounts

---
+++ src/app/pages/main-register-user-accounts/main-register-user-accounts.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-register-user-accounts',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      main-register-user-accounts works!
+    </p>
+  `,
+  styleUrl: './main-register-user-accounts.component.css'
+})
+export class MainRegisterUserAccountsComponent {
+
+}
