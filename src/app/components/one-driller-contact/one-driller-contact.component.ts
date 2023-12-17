77346ffdeabb5a60cf4dfa6444a213a51a3c6adf
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-17 05:50:46 +0200

    finished about one driller

---
+++ src/app/components/one-driller-contact/one-driller-contact.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-driller-contact',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      one-driller-contact works!
+    </p>
+  `,
+  styleUrl: './one-driller-contact.component.css'
+})
+export class OneDrillerContactComponent {
+
+}
