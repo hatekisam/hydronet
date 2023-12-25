f9d08d0f130efe258a51bafa0cf2a6a888041104
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-25 20:27:52 +0200

    made the permit certifcatis tabs

--- src/app/pages/main-register-permit-certificates/main-register-permit-certificates.component.ts
+++ src/app/pages/main-register-permit-certificates/main-register-permit-certificates.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { RegDriPerCer1Component } from '../../components/mainRegister/drillerPermits-certificates/reg-dri-per-cer1/reg-dri-per-cer1.component';
+import { RegDriPerCer2Component } from '../../components/mainRegister/drillerPermits-certificates/reg-dri-per-cer2/reg-dri-per-cer2.component';
+import { RegDriPerCer3Component } from '../../components/mainRegister/drillerPermits-certificates/reg-dri-per-cer3/reg-dri-per-cer3.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-register-permit-certificates',
   standalone: true,
-  imports: [],
+  imports: [
+    RegDriPerCer1Component,
+    RegDriPerCer2Component,
+    RegDriPerCer3Component,
+    CommonModule,
+  ],
   template: `
-       <div class="h-full">
+    <div class="h-full">
       <div *ngIf="activeTab == 0" class="h-full">
-        <app-main-reg-comp1  />
+        <reg-dri-per-cer1 />
       </div>
       <div *ngIf="activeTab == 1">
-        <app-main-reg-comp2  />
+        <reg-dri-per-cer2 />
       </div>
       <div *ngIf="activeTab == 3">
-        <app-main-reg-comp3  />
+        <reg-dri-per-cer3 />
       </div>
       <!-- <div *ngIf="activeTab == 4">
         <app-main-reg-comp4 [changeTab]="changeTab" />
